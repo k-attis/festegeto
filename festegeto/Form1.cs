10601f87c671de7a87a649c2c8f1d2df6cc7c0e8
Kalcsú Attila <attila.kalcsu@gmail.com>
2017-02-16 09:19:30 +0100

    szál is létrehozva, form nem méretezhető

--- festegeto/Form1.cs
+++ festegeto/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -15,6 +16,37 @@ namespace festegeto
         public Form1()
         {
             InitializeComponent();
+        }
+
+        Bitmap buffer;
+        Graphics bufferg;
+
+        private void panel2_Resize(object sender, EventArgs e)
+        {
+            buffer = new Bitmap(panel2.Width, panel2.Height);
+            bufferg = Graphics.FromImage(buffer);
+        }
+
+        Thread t;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+
+            t = new Thread(new ThreadStart(szal));
+            t.Start();
+        }
+
+        void szal()
+        {
+            bufferg.Clear(Color.White);
+
+            for (int y = 0; y < buffer.Height; y++)
+                for (int x = 0; x < buffer.Width; x++)
+                    if ((y * buffer.Width + x) % 8 == 1)
+                        buffer.SetPixel(x, y, Color.Black);
+
+            button1.Enabled = true;
         }
     }
 }
